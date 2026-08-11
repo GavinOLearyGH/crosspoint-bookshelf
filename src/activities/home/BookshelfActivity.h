a551131000b8180d059aba726944c8179d33a93d
Gavin O'Leary <gavinoleary@gmail.com>
2026-08-11 16:40:32 -0400

    feat(bookshelf): make bookshelf a persisted book list

--- src/activities/home/BookshelfActivity.h
+++ src/activities/home/BookshelfActivity.h
@@ -1,13 +1,29 @@
 #pragma once
 
-#include "activities/Activity.h"
+#include <string>
+#include <vector>
 
-class BookshelfActivity final : public Activity {
+#include "RecentBooksStore.h"
+#include "activities/UiListActivity.h"
+
+class BookshelfActivity final : public UiListActivity {
  public:
-  BookshelfActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
-      : Activity("Bookshelf", renderer, mappedInput) {}
-
+  explicit BookshelfActivity(GfxRenderer& renderer, MappedInputManager& mappedInput);
   void onEnter() override;
-  void loop() override;
-  void render(RenderLock&&) override;
+  void onExit() override;
+
+ private:
+  int listCount() const override { return static_cast<int>(books.size()); }
+  void buildScreen(UiScreen& screen) override;
+  void activateIndex(int index) override;
+  void onRowLongPress(int index) override;
+  bool handleButtons() override;
+  const char* headerTitle() const override { return "Bookshelf"; }
+  void drawFooter() override;
+
+  std::vector<RecentBook> books;
+  bool longPressFired = false;
+
+  void loadBooks();
+  void promptRemoveBook(const std::string& path, const std::string& title);
 };
